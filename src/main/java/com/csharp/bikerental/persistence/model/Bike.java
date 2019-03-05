b2ae229fc4aed852f27d02c8ca0f7edb3cc8fae1
conorH22 <lackamoredude@gmail.com>
2019-03-05 17:58:54 +0000

    WIP: just created model classes

---
+++ src/main/java/com/csharp/bikerental/persistence/model/Bike.java
@@ -0,0 +1,9 @@
+package com.csharp.bikerental.persistence.model;
+
+public interface Bike {
+
+    public void ID();
+    public void Condition();
+    public void Select();
+
+}
---
+++ src/main/java/com/csharp/bikerental/persistence/model/EBike.java
@@ -0,0 +1,21 @@
+package com.csharp.bikerental.persistence.model;
+
+import javax.persistence.Entity;
+
+
+public class EBike implements Bike {
+    @Override
+    public void Select() {
+
+    }
+
+    @Override
+    public void ID() {
+
+    }
+
+    @Override
+    public void Condition() {
+
+    }
+}
